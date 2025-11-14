56f2b932ce009b601d8cbb270c21ab1bb936c421
Shiva Mani <mop91838@gmail.com>
2025-11-14 16:14:10 +0000

    001

---
+++ day3.cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+  // char ch ;
+  // cout << "enter  a char :";
+  // cin >> ch ;
+  // if ( ch >= 65  && ch <= 90 ){
+  //     cout << "upper case\n";
+  //  } else
+  //     cout << "lower case\n";
+
+
+
+
+  // int n = 2;
+  // cout << ( n>=2 && n<=100 ? "y" : "n") << endl;
+
+
+
+
+
+
+
+//  int n = 0x221E;
+ 
+  
+//   // while (count <= n){
+//   //   cout << count << " " ;
+//   //   count++;
+//   // };
+// for ( int i=1; i<=n ; i++){
+//   cout << i << " ";
+// }
+
+
+
+
+
+
+
+// sum of number form 1 to n
+//   int n;
+//   int count = 0;
+//   cout << "enter a nuber";
+//   cin >> n ;
+
+
+
+
+//   for(int i=0; i<=n; i++ ){
+//     if( i % 2 != 0) count += i;
+//   }
+ 
+// cout << count;
+
+
+
+
+
+
+
+// int n= 10;
+// int i= 1;
+// do {
+//   cout<< " hello";
+//   cout << i;
+//   i++;
+// }while (n>i);
+
+
+
+
+
+
+// int n, i;
+// bool isprime = true;
+// cout << "enter a number";
+// cin >> n;
+// for (i=2; i*i <= n; i++){
+//   if( n % i == 0 ){
+//      isprime= false;
+    
+//   } 
+// }
+// cout<< isprime;
+
+
+
+
+
+
+
+// int n, i, j;
+// cout << "Enter a number: ";
+// cin >> n;
+
+
+// for (i = 2; i <= n; i++) {
+//   int count = 1;
+
+//   for (j = 2; j <= i - 1; j++) {
+        
+//     if (i % j == 0) {
+//       count = 0;
+//       break;
+//     } 
+//   }
+//   if(count == 1){
+//     cout << i << " ";
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ return 0;
+}
---
+++ day4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+
+
+    // int j, i, n = 5;
+    // for (i=1; i<= n-1; i++){
+    //     for (j=1; j<=n-1; j++){
+    //         cout<< j ;
+    //     }
+    //     cout<< endl;
+    // }
+  int i, j;
+    int n = 5;
+    char c = 'A';  // initialize character
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            cout << c << " ";
+            c = c + 1;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
