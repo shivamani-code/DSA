bae3a4810830a7f72dba0ad788740948b32dd9cd
Shiva Mani <mop91838@gmail.com>
2026-02-15 13:06:01 +0000

    maxsubarray

--- maxsubarray.cpp
+++ maxsubarray.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main(){
     int n=5;
     int a[5]={1,2,3,4,5};
+    int ms=0;
     for (int st=0; st<n; st++){
+        int cs=0;
         for (int end=st; end<n; end++){
-            for(int i =st; i<=end; i++){
-                cout<<a[i];
-            }
-            cout<<" ";
+            cs+=a[end];
+            ms=max(cs,ms);
         }
-        cout<<endl;
     }
+    cout<<ms<<endl;
     return 0;
 }
